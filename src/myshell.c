434260e3250d5bae8206bb339b9249ae8719c8a4
HDG <hdg202101693@gmail.com>
2024-12-04 14:10:55 +0900

    parsing commands is added

---
+++ src/myshell.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX_LINE 80
+#define MAX_ARGS 10
+int main()
+{
+    char input[MAX_LINE];
+    char *token;
+    char *argv[MAX_ARGS];
+
+    while(1) {
+        printf("myshell> ");
+        fgets(input, MAX_LINE, stdin);
+        printf("%s\n", input);
+        token = strtok(input, " \n\t");
+        int i = 0;
+        while (token != NULL) {
+            argv[i++] = token;
+            token = strtok(NULL, " \n\t");
+        }
+        argv[i] = NULL;
+
+        if(argv[0] == NULL) {
+            continue;
+        }
+        
+        if (strcmp(argv[0], "exit") == 0){
+            printf("Goodbye.\n");
+            exit(0);
+        }
+    }
+
+    return 0;
+}
